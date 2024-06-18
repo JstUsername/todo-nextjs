f76df68896e049377a1ca2f24be559f8b70d8eec
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-06-18 16:54:49 +0300

    fix(page.js):
    переменная task вынесена в стейт, изменено наименование стейта, добавлены стейты и функции модального окна добавления задачи

--- app/page.js
+++ app/page.js
@@ -1,31 +1,35 @@
 'use client';
-
 import { styled } from '@mui/material/styles';
 import Head from 'next/head';
 import AppNavBar from './components/NavBar';
 import TaskList from './components/TaskList';
 import { useState } from 'react';
-
-let tasks = [];
+import ModalAdd from '@/app/components/ModalAdd';
 
 export default function Root() {
-  const [value, setValue] = useState('');
-  const handleTask = (value) => {
-    setValue(value);
-    tasks = [...tasks, value];
+  const [complete, setComplete] = useState([]);
+  const [task, setTask] = useState('');
+  const handleTask = (task) => {
+    setTask(task);
+    setComplete([...complete, task]);
   };
+
   const [search, setSearch] = useState('');
-  const handleSearch = (query) => {
-    setSearch(query);
-  };
+  const handleSearch = (query) => setSearch(query);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="any" />;
       </Head>
       <Page>
-        <AppNavBar onClick={handleTask} onChange={handleSearch} />
-        <TaskList data={tasks} searchQuery={search} />
+        <AppNavBar handleOpen={handleOpen} />
+        <ModalAdd onClick={handleTask} handleClose={handleClose} open={open} />
+        <TaskList complete={complete} setComplete={setComplete} searchQuery={search} />
       </Page>
     </>
   );
