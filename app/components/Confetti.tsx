a1b43d8bf7cda1fb6685c4f4529e77258bb94b08
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-06-25 03:59:18 +0300

    fix(Confetti.tsx): переименован интерфейс пропсов

--- app/components/Confetti.tsx
+++ app/components/Confetti.tsx
@@ -2,11 +2,11 @@ import { useWindowSize } from '@react-hook/window-size';
 import dynamic from 'next/dynamic';
 const FallingConfetti = dynamic(() => import('react-confetti'), { ssr: false });
 
-interface Props {
+interface ConfettiProps {
   run: boolean;
 }
 
-export default function Confetti({ run }: Props) {
+export default function Confetti({ run }: ConfettiProps) {
   const [width, height] = useWindowSize();
   return <FallingConfetti width={width} height={height} run={run} recycle={false} gravity={0.25} />;
 }
